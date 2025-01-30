e126fab4c7fa61671745dbb1c671f5ac17b4d80d
ThiagoMachado2 <136006071+ThiagoMachado2@users.noreply.github.com>
2025-01-30 10:23:41 -0300

    🐛 FIX: Corrigindo as pinagens para ficar correspondente com a placa BitDogLab
    🐛 FIX: Corrigindo as pinagens para ficar correspondente com a placa BitDogLab

--- atividade1-temporizador-periodico/atividade1-temporizador-periodico.c
+++ atividade1-temporizador-periodico/atividade1-temporizador-periodico.c
@@ -2,9 +2,9 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-#define LED_VERMELHO 11
+#define LED_VERMELHO 13
 #define LED_AMARELO 12
-#define LED_VERDE 13
+#define LED_VERDE 11
 
 volatile int estado = 0;
 
@@ -52,7 +52,7 @@ int main()
 
     struct repeating_timer timer;
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
-
+    
     while (1)
     {
         sleep_ms(1000);
